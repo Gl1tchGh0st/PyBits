2d7ef45a2180481f03450990cf4fc129e469e271
r3dwh33lb4rrow <132740094+r3dwh33lb4rrow@users.noreply.github.com>
2024-01-10 12:10:05 +1030

    Create Square-Root.py

---
+++ Square-Root.py
@@ -0,0 +1,8 @@
+# Program to solve for square root
+def square_root(num):
+    print(int((num) ** 0.5))
+
+choice = int(input("Enter the number to find the square root for: "))
+
+square_root(choice)
+input("")
